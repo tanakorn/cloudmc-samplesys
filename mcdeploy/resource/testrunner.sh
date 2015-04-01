6d9a6afdadeaa402a1ee637576d56c96c05dee55
Tanakorn L <tanakorn@cs.uchicago.edu>
2015-04-01 16:43:23 -0500

    Fix wrong path, I have to find a good way to identify path

--- mcdeploy/resource/testrunner.sh
+++ mcdeploy/resource/testrunner.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-leader_election=/Users/tanakorn/Documents/workspace/AutoSamc
+leader_election=/Users/tanakorn/Documents/UChicago/SAMC/simc
 classpath=.:$leader_election/bin
 lib=$leader_election/lib
 for j in `ls $lib/*.jar`; do
@@ -9,5 +9,5 @@ done
 export CLASSPATH=$CLASSPATH:$classpath
 export PATH=$PATH:bin/
 
-java -Dsun.rmi.dgc.cleanInterval=10000 -Dsun.rmi.dgc.server.gcInterval=10000 -Dlog4j.configuration=mc_log.properties -Delectiontest.dir=WORKING_DIR mc.election.LeaderElectionTestRunner ./mc.conf
+java -Dsun.rmi.dgc.cleanInterval=10000 -Dsun.rmi.dgc.server.gcInterval=10000 -Dlog4j.configuration=mc_log.properties -Delectiontest.dir=WORKING_DIR edu.uchicago.cs.ucare.simc.election.LeaderElectionTestRunner ./mc.conf
 
